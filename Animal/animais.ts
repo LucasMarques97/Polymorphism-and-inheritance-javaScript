42c27a55ac76813dde0fb1015837f0a8886a89ef
LucasMarques97 <111980558+LucasMarques97@users.noreply.github.com>
2022-09-23 10:03:37 -0300

    Update animais.ts

--- Animal/animais.ts
+++ Animal/animais.ts
@@ -15,27 +15,28 @@ export class animais {
         this.emitirSom = emitirSom
         this.corre = corre
     }
-    mostrarChachorro(){
-            //console.log(`O cachorro de nome ${this.nome} e idade ${this.idade} latiu: ${this.emitirSom} , e depois ${this.corre}`)
+       mostrarChachorro(){
+            console.log(`O cachorro de nome ${this.nome}, possui ${this.idade} anos de idade, quando avistou o carteiro passando na rua de bicicleta logo rosnou e começou a latir "${this.emitirSom}", e começou a perseguir o carteiro até ${this.corre}\n`)
     }
+    
     mostrarPreguica(){
-            //console.log(`A preguiça de nome ${this.nome} e idade ${this.idade} emitiu som equivalente a ${this.emitirSom}, e para correr ${this.corre}`)
+            console.log(`A preguiça de nome ${this.nome}, possui ${this.idade} anos de idade, ao subir uma árvore emitiu um som "${this.emitirSom}", se assustou mas ${this.corre} para correr\n`)
     }
+    
     mostrarCavalo(){
-           // console.log(`O cavalo de nome ${this.nome} e idade ${this.idade} emitiu som equivalente a${this.emitirSom} , e corre ${this.corre}`)
-           console.log(`O cavalo de nome ${this.nome} e idade ${this.idade} emitiu som equivalente a${this.emitirSom} , e corre ${this.corre}`)
+           console.log(`O cavalo de nome ${this.nome}, possui ${this.idade} anos, emitiu som equivalente a "${this.emitirSom}" , pois corria ${this.corre}\n`)
     }
   
 }
 
-var animal1 = new animais ("Felipe Mello", 30, "kraus,kraus", "correndo..., cansou...., parou..., deitou.. e dormiu no meio da rua")
-animal1.mostrarChachorro()
+export var animal1 = new animais ("Felipe Mello", 30, "kraus,kraus", "cansar...., deitar.., e dormir no meio da rua")
+// animal1.mostrarChachorro()
 
-var animal2 = new animais ("Jão", 20, "tata", "paum")
-animal2.mostrarPreguica()
+export var animal2 = new animais ("Vitinho", 200, "Pum Pum Pum", "não possui energia")
+//animal2.mostrarPreguica()
 
-var animal3 = new animais ("pao", 30, "jajajaj", "tuabjd")
-animal3.mostrarCavalo()
+export var animal3 = new animais ("Arrascaeta", 30, "Vrum Vrum", "Veloz como foguete")
+// animal3.mostrarCavalo()
 
 
 
